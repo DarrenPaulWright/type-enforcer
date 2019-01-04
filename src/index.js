1204054f6e179202943eec8beee2f0fa5c88b66f
DarrenPaulWright <darren@darrenpaulwright.com>
2019-01-04 15:30:23 -0800

    Adding example of babel.config.js to readme

--- src/index.js
+++ src/index.js
@@ -11,6 +11,26 @@
  *
  * This library uses [@babel/plugin-proposal-class-properties](https://github.com/babel/babel/tree/master/packages/babel-plugin-proposal-class-properties) and [@babel/plugin-proposal-private-methods](https://github.com/babel/babel/tree/master/packages/babel-plugin-proposal-private-methods) so you will need to set up a [babel.config.js](https://babeljs.io/docs/en/config-files#project-wide-configuration) file and set the [rootMode](https://babeljs.io/docs/en/options#rootmode) option to 'upward'.
  *
+ * Example:
+ * ``` javascript
+ * // babel.config.js
+ * module.exports = function(api) {
+ * 	const presets = [['@babel/preset-env']];
+ * 	const plugins = [
+ * 		'lodash',
+ * 		['@babel/plugin-proposal-private-methods', {'loose': true}],
+ * 		['@babel/plugin-proposal-class-properties', {'loose': true}]
+ * 	];
+ *
+ * 	api.cache(true);
+ *
+ * 	return {
+ * 		presets,
+ * 		plugins
+ * 	};
+ * };
+ * ```
+ *
  * ## Contributing
  *
  * If you would like to help out, type-enforcer still needs full test coverage and more data types. If you add a new type be sure to add an enforce function and a method. When done, add any new types to package.json scripts, docs-newType, and add that to the docs script, then run
